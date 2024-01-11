d0cfd54ba65824625f362f276811d0bc2e8240ea
Ucha <uchajaniashvili8@gmail.com>
2024-01-11 19:14:27 +0400

    fix script

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, Origin, X-Requested-With, Accept"
   );
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
